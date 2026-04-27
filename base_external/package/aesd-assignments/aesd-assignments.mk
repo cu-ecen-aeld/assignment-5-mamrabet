751bc045b0ff40e132776112d6ef3743768e0d32
Marouene Mrabet <mrabet.marouene@gmail.com>
2026-04-27 14:26:12 +0200

    Assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '164eceb8e0832dc71637382bfd0de7945706cce1'
+AESD_ASSIGNMENTS_VERSION = '8a300894bd4ca9282513ad42aa2b5e8ef4c4b136'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DRIVER_VERSION = c687e7d970d63935a8e9da73f3b055df04cd2a6f
+AESD_CHAR_DRIVER_VERSION = 8a300894bd4ca9282513ad42aa2b5e8ef4c4b136
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
